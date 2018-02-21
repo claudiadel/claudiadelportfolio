ff08035c7f1f34ec6fbef80f857e533b0c551fb8
claudiadel <cdelbarr@gmail.com>
2018-02-21 08:53:17 +0100

    container-fluid
    erased  <div id="content" class="container-fluid">

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php include("inc/head.php");?>
 	<div id="content" class="container-fluid">
-		<div class="row">
 			<div class="video-header wrap">
 				<div class="fullscreen=video-wrap">	
 					<video src="video/inicio.mp4" autoplay loop></video>
@@ -10,6 +9,5 @@
 					<h1>Clàudia Del</h1>
 				</div>
 			</div>	
-		</div>
 	</div>
 <?php include("inc/footer.php");?>
