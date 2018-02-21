4541b1f6c43395018a571cbb1500acce90d5b828
claudiadel <cdelbarr@gmail.com>
2018-02-21 08:49:38 +0100

    Inicio_video
    Added video following this tutorial steps:
    https://www.youtube.com/watch?v=Y1eCNrUDrQg

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php include("inc/head.php");?>
 	<div id="content" class="container-fluid">
-		<div class="row">			
+		<div class="row">
+			<div class="video-header wrap">
+				<div class="fullscreen=video-wrap">	
+					<video src="video/inicio.mp4" autoplay loop></video>
+				</div>
+				<div class="header-overlay">
+					<div class="header-content">
+					<h1>Clàudia Del</h1>
+				</div>
+			</div>	
 		</div>
 	</div>
 <?php include("inc/footer.php");?>
